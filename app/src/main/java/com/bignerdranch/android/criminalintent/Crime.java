7fe3337d905d0dd011cc8e13e1146495d064272c
Milan Horvat <milan-horvat@siol.net>
2015-10-30 21:21:51 +0100

    fff

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -30,7 +30,7 @@ public class Crime {
     private boolean mSolved;
 
     public Crime() {
-        // Generate unique identifier aaa
+        // Generate unique identifier aaasd
         mId = UUID.randomUUID();
         mDate = new Date();
     }
